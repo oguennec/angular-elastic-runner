f155814609b1bc537f095f1b9133e7883d166e3f
Olivier Guennec <oguennec@gmail.com>
2015-11-12 14:49:17 +0000

    change express listen port to 8086 on develop branch

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require('path')
 var app = express();
-var port = 8089;
+var port = 8086;
 
 app.use(express.static(path.join(__dirname + '/public')));
 app.use(express.static(path.join(__dirname + '/public-lib')));
